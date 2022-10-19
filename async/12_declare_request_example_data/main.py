a99d96455e0fedc1aa8f6c5ab19348f25a292df2
Sangjun Cha <sangjuncha.dev@gmail.com>
2022-10-20 00:13:12 +0900

    🔨 Add Declare Request Example Data

---
+++ async/12_declare_request_example_data/main.py
@@ -0,0 +1,110 @@
+from typing import Union
+
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+# Pydantic schema_extra
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item": item}
+
+
+# Field additional arguments
+class Item2(BaseModel):
+    name: str = Field(example="Foo")
+    description: Union[str, None] = Field(default=None, example="A very nice Item")
+    price: float = Field(example=35.4)
+    tax: Union[float, None] = Field(default=None, example=3.2)
+
+
+@app.put("/items2/{item_id}")
+async def update_item2(item_id: int, item: Item2):
+    return {"item_id": item_id, "item": item}
+
+
+# Body with example
+class Item3(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+@app.put("/items3/{item_id}")
+async def update_item3(
+    item_id: int,
+    item: Item = Body(
+        example={
+            "name": "Foo",
+            "description": "A very nice Item",
+            "price": 35.4,
+            "tax": 3.2,
+        },
+    ),
+):
+    return {"item_id": item_id, "item": item}
+
+
+# Body with multiple examples
+class Item4(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+@app.put("/items4/{item_id}")
+async def update_item4(
+    *,
+    item_id: int,
+    item: Item = Body(
+        examples={
+            "normal": {
+                "summary": "A normal example",
+                "description": "A **normal** item works correctly.",
+                "value": {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                },
+            },
+            "converted": {
+                "summary": "An example with converted data",
+                "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                "value": {
+                    "name": "Bar",
+                    "price": "35.4",
+                },
+            },
+            "invalid": {
+                "summary": "Invalid data is rejected with an error",
+                "value": {
+                    "name": "Baz",
+                    "price": "thirty five point four",
+                },
+            },
+        },
+    ),
+):
+    return {"item_id": item_id, "item": item}
