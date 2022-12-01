30b6fb493850504eaba540178e960adb33e9d58b
Sangjun Cha <sangjuncha.dev@gmail.com>
2022-12-01 22:31:49 +0900

    🔨 Add Advanced Dependencies

---
+++ async/05_advanced_user_guide/10_advanced_dependencies/main.py
@@ -0,0 +1,26 @@
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+
+class FixedContentQueryChecker:
+    # Parameterize the instance
+    def __init__(self, fixed_content: str):
+        self.fixed_content = fixed_content
+
+    # A "callable" instance
+    def __call__(self, q: str = ""):
+        if q:
+            return self.fixed_content in q
+        return False
+
+
+# Create an instance
+checker = FixedContentQueryChecker("bar")
+
+
+# Use the instance as a dependency
+@app.get("/query-checker/")
+async def read_query_check(fixed_content_included: bool = Depends(checker)):
+    return {"fixed_content_in_query": fixed_content_included}
+
---
+++ sync/05_advanced_user_guide/10_advanced_dependencies/readme.md
@@ -0,0 +1,18 @@
+# Advanced Dependencies
+
+고급 종속성
+
+
+## A "callable" instance
+
+- __call__: 추가 매개변수 및 하위 종속성을 확인하는 데 사용하고 나중에 경로 작업 함수 의 매개변수에 값을 전달하기 위해 호출되는 것
+
+
+## Parameterize the instance
+
+- __init__: 종속성을 "매개 변수화"하는 데 사용할 수 있는 인스턴스의 매개 변수를 선언하는 데 사용
+
+
+## 참고 Docs
+
+- https://fastapi.tiangolo.com/advanced/advanced-dependencies/
