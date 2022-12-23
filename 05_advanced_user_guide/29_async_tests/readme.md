ba83ae0fe464a8574c68d5714a92d9177b5f9af2
sj.cha <sj.cha@psx.or.kr>
2022-12-23 23:21:05 +0900

    🔨 Add Async Tests

---
+++ 05_advanced_user_guide/29_async_tests/readme.md
@@ -0,0 +1,25 @@
+# Async Tests
+
+비동기 테스트
+
+
+## pytest.mark.anyio
+
+테스트에서 비동기 함수를 호출하려면 테스트 함수가 비동기여야함
+
+
+## HTTPX
+
+비동기 테스트 클라이언트 모듈
+
+
+## 라이브러리 설치
+
+```
+pip install trio
+```
+
+
+## 참조 Docs
+
+- https://fastapi.tiangolo.com/advanced/async-tests/
---
+++ 05_advanced_user_guide/29_async_tests/test_main.py
@@ -0,0 +1,14 @@
+import pytest
+from httpx import AsyncClient
+
+from main import app
+
+
+# In Detail
+@pytest.mark.anyio
+async def test_root():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Tomato"}
